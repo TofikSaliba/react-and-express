2efd10a0b309c08c07b8c98dc14fdf7d5e9904d8
TofikSaliba <tofik.saliba@gmail.com>
2022-06-15 22:33:33 +0300

    git add .

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production" ? "/" : "http://localhost:5000",
+  baseURL: "http://localhost:5000",
 });
+
+// process.env.NODE_ENV === "production" ? "/" : "http://localhost:5000",
